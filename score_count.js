12574e099d08b8451c66c0e23adc058eb3c60837
jeffreytseng3 <jeffrey.tseng3@gmail.com>
2019-06-13 17:55:46 -0400

    get destroyed counter working

--- score_count.js
+++ score_count.js
@@ -10,13 +10,20 @@ function drawScore() {
         }
     }
 
-    
+    let enemySum = 0; 
+    for (let i = 0; i < ships.length; i++) {
+        let ship = ships[i];
+        if (ship.destroyed) {
+            enemySum += 1;
+        }
+    }
 
+    let total = (ringsSum + enemySum).toString();
 
-
-
-    let score = 'score: ' + ringsSum.toString();
-    text(score, width - 70, 100);
+    let ringScore = 'rings: ' + ringsSum.toString();
+    let enemyScore = 'destroyed: ' + enemySum.toString();
+    text(ringScore, width - 90, 100);
+    text(enemyScore, width - 90, 120);
     fill(255,255,255);
     textSize(14);
 }
